fd3f71e6eab1afdfbb25cc59323b964f71071ca7
Brian Schroer <schroer.brian@gmail.com>
2013-08-12 18:31:34 -0700

    Added background image and did styling of greeting section

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Portfolio::Application.config.secret_key_base = 'adf7909f8864eb87a7a36cb5f04de51434b78e49aa887e38067a382109e2cd79ac8febec9d80b1f7ddbeebc605d103955ea97292eb87cbaef06776a46dae95f0'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Portfolio::Application.routes.draw do
+ get '/' => 'home#index'
+end
