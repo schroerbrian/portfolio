24176161ad9c81df25cf219da8eac4db5ab0d796
Brian Schroer <schroer.brian@gmail.com>
2013-09-18 09:37:49 -0700

    responsive and jq

--- app/assets/javascripts/work.js
+++ app/assets/javascripts/work.js
@@ -1,30 +1,46 @@
-$(document).ready(function() {
-  
-  //TAILOR
-  $('.tailor-image').click(function(event){
-    event.preventDefault();
-    $('.tailor-modal').fadeIn(500);
-    $('.overlay').fadeIn(500);
-  })
+var ready;
 
-  $('.remove-tailor').click(function(event){
+ready = function() {
+  //TAILOR
+  $('.tailor-anchor').on('click', function(event){
     event.preventDefault();
-    $('.tailor-modal').fadeOut(400);
-    $('.overlay').fadeOut(400);
-  })
+    $('.tailor-modal').fadeIn(300);
+    $('.overlay').fadeIn(300);
+  });
+
+  $('.remove-tailor').on('click', function(event){
+    event.preventDefault();
+    $('.tailor-modal').fadeOut(300);
+    $('.overlay').fadeOut(300);
+  });
 
   //LOCALE
-  $('.locale-image').click(function(event){
+  $('.locale-anchor').on('click', function(event){
     event.preventDefault();
-    $('.locale-modal').fadeIn(500);
-    $('.overlay').fadeIn(500);
-  })
+    $('.locale-modal').fadeIn(300);
+    $('.overlay').fadeIn(300);
+  });
 
-  $('.remove-locale').click(function(event){
+  $('.remove-locale').on('click', function(event){
     event.preventDefault();
-    $('.locale-modal').fadeOut(400);
-    $('.overlay').fadeOut(400);
-  }) 
+    $('.locale-modal').fadeOut(300);
+    $('.overlay').fadeOut(300);
+  });
 
-});
+  //GROOVES
+  $('.grooves-anchor').on('click', function(event){
+    event.preventDefault();
+    $('.grooves-modal').fadeIn(300);
+    $('.overlay').fadeIn(300);
+  });
+
+  $('.remove-grooves').on('click', function(event){
+    $('.grooves-modal').fadeOut(300);
+    $('.overlay').fadeOut(300);
+  });
+}; 
+
+$(document).ready(ready);
+
+$(document).on('page:load', ready);
 
