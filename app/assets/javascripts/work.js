edcf3c5886b4c42230034e50c202785c212b8519
Brian Schroer <schroer.brian@gmail.com>
2013-09-24 21:27:33 -0700

    weird js

--- app/assets/javascripts/work.js
+++ app/assets/javascripts/work.js
@@ -1,15 +1,19 @@
+'use strict';
+
 var ready;
 
 ready = function() {
   //TAILOR
   $('.tailor-anchor').on('click', function(event){
     event.preventDefault();
+    $('.work-examples-wrapper').show();
     $('.tailor-modal').fadeIn(300);
     $('.overlay').fadeIn(300);
   });
 
   $('.remove-tailor').on('click', function(event){
     event.preventDefault();
+    $('.work-examples-wrapper').hide();
     $('.tailor-modal').fadeOut(300);
     $('.overlay').fadeOut(300);
   });
@@ -17,12 +21,14 @@ ready = function() {
   //LOCALE
   $('.locale-anchor').on('click', function(event){
     event.preventDefault();
+    $('.work-examples-wrapper').show();
     $('.locale-modal').fadeIn(300);
     $('.overlay').fadeIn(300);
   });
 
   $('.remove-locale').on('click', function(event){
     event.preventDefault();
+    $('.work-examples-wrapper').hide();
     $('.locale-modal').fadeOut(300);
     $('.overlay').fadeOut(300);
   });
@@ -30,11 +36,13 @@ ready = function() {
   //GROOVES
   $('.grooves-anchor').on('click', function(event){
     event.preventDefault();
+    $('.work-examples-wrapper').show();
     $('.grooves-modal').fadeIn(300);
     $('.overlay').fadeIn(300);
   });
 
   $('.remove-grooves').on('click', function(event){
+    $('.work-examples-wrapper').hide();
     $('.grooves-modal').fadeOut(300);
     $('.overlay').fadeOut(300);
   });
