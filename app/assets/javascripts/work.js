7517d05455ad578a4c036dccba8b9f152bb0d111
Brian Schroer <schroer.brian@gmail.com>
2013-09-25 23:28:20 -0700

    remove show modal from js. just need to show wrapper. modal is included

--- app/assets/javascripts/work.js
+++ app/assets/javascripts/work.js
@@ -3,18 +3,21 @@
 var ready;
 
 ready = function() {
+  
+  //INITIALIZE MODALS. REMOVE THEM
+  // function initializeModal(clickedClass, )
   //TAILOR
   $('.tailor-anchor').on('click', function(event){
     event.preventDefault();
     $('.work-examples-wrapper-t').css('display', 'block');
-    $('.tailor-modal').fadeIn(300);
+    // $('.tailor-modal').fadeIn(300);
     $('.overlay').fadeIn(300);
   });
 
   $('.remove-tailor').on('click', function(event){
     event.preventDefault();
     $('.work-examples-wrapper-t').css('display', 'none');
-    $('.tailor-modal').fadeOut(300);
+    // $('.tailor-modal').fadeOut(300);
     $('.overlay').fadeOut(300);
   });
 
@@ -22,14 +25,14 @@ ready = function() {
   $('.locale-anchor').on('click', function(event){
     event.preventDefault();
     $('.work-examples-wrapper-l').css('display', 'block');
-    $('.locale-modal').fadeIn(300);
+    // $('.locale-modal').fadeIn(300);
     $('.overlay').fadeIn(300);
   });
 
   $('.remove-locale').on('click', function(event){
     event.preventDefault();
     $('.work-examples-wrapper-l').css('display', 'none');
-    $('.locale-modal').fadeOut(300);
+    // $('.locale-modal').fadeOut(300);
     $('.overlay').fadeOut(300);
   });
 
@@ -37,15 +40,21 @@ ready = function() {
   $('.grooves-anchor').on('click', function(event){
     event.preventDefault();
     $('.work-examples-wrapper-g').css('display', 'block');
-    $('.grooves-modal').fadeIn(300);
+    // $('.grooves-modal').fadeIn(300);
     $('.overlay').fadeIn(300);
   });
 
   $('.remove-grooves').on('click', function(event){
     $('.work-examples-wrapper-g').css('display', 'none');
-    $('.grooves-modal').fadeOut(300);
+    // $('.grooves-modal').fadeOut(300);
     $('.overlay').fadeOut(300);
   });
+
+  //CYCLE THROUGH MODALS
+
+
+
+
 }; 
 
 $(document).ready(ready);
